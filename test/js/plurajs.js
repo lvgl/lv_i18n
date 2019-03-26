96678f7446d8c8f5106a8ad8b36da96c1664f604
Vitaly Puzrin <vitaly@rcdesign.ru>
2019-03-26 11:39:04 +0300

    Fix duplicated `uint32_t n` in some plurals

--- test/js/plurajs.js
+++ test/js/plurajs.js
@@ -2,9 +2,16 @@
 
 
 const create_plural = require('../../lib/plurals').create_c_plural_fn;
+const assert        = require('assert');
 
 
 describe('Plurals', function () {
+
+  it('should not add "uint32_t n = op_n(n); UNUSED(n);"', function () {
+    let fn_body = create_plural('tr', 'p').toString();
+
+    assert.ok(!fn_body.includes('uint32_t n = op_n(n); UNUSED(n);'));
+  });
 
   // Just run all branches without result check. Generator code is taken
   // from well-tested https://github.com/nodeca/plurals-cldr. So, everything
